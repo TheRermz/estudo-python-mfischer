787a3f015ee2000e895bbace4bd67be935abcd6f
Murilo Fischer <mufischer@hotmail.com>
2024-04-28 01:07:13 -0300

    Módulos e Funções

--- sabor-express/app.py
+++ sabor-express/app.py
@@ -1,13 +1,42 @@
-print("Sabor Express\n")
+import os
 
-print("1. Cadastrar restaurante")
-print("2. Listar restaurante")
-print("3. Ativar restaurante")
-print("3. Sair\n")
+def exibir_nome_programa():
+    print("Sabor Express\n")
 
-opcao_escolhida = input("Escolha uma opção: ")
-print("Você escolheu a opção", opcao_escolhida)
-# fstring --> interpolação de strings
-print(f"Você escolheu a opção {opcao_escolhida}")
+def exibir_opcoes():
+    print("1. Cadastrar restaurante")
+    print("2. Listar restaurante")
+    print("3. Ativar restaurante")
+    print("4. Sair\n")
 
-str porra = 'oolar'
+def finalizar_app():
+    os.system("clear")
+    # os.system("cls") para windows
+    print("Finalizando App")
+
+def escolher_opcao():
+    opcao_escolhida = int(input("Escolha uma opção: "))
+    print("Você escolheu a opção", opcao_escolhida)
+    # fstring --> interpolação de strings
+    print(f"Você escolheu a opção {opcao_escolhida}")
+
+
+
+    if opcao_escolhida == 1:
+        print("Cadastrar restaurante")
+    elif opcao_escolhida == 2:
+        print("Listar restaurantes")
+    elif opcao_escolhida == 3:
+        print("Ativar restaurante")
+    else:
+        finalizar_app()
+
+def main():
+    exibir_nome_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+
+
+if __name__ == "__main__":
+    main()
