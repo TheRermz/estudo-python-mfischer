0bc619b9ce6d68153f6a81965a42652b31fb130e
Murilo Fischer <mufischer@hotmail.com>
2024-04-28 00:48:44 -0300

    Manipulando Strings

---
+++ exercicios/manipulando-strings/nome_idade.py
@@ -0,0 +1,4 @@
+nome = "Murilo"
+idade = 26
+
+print(f"Meu nome é {nome} e tenho {idade} anos")
---
+++ sabor-express/app.py
@@ -0,0 +1,13 @@
+print("Sabor Express\n")
+
+print("1. Cadastrar restaurante")
+print("2. Listar restaurante")
+print("3. Ativar restaurante")
+print("3. Sair\n")
+
+opcao_escolhida = input("Escolha uma opção: ")
+print("Você escolheu a opção", opcao_escolhida)
+# fstring --> interpolação de strings
+print(f"Você escolheu a opção {opcao_escolhida}")
+
+str porra = 'oolar'
