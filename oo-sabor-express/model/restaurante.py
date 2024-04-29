b0747260b81c62293c8a478d95cdbe01032b2fea
Murilo Fischer <mufischer@hotmail.com>
2024-04-28 22:01:39 -0300

    property e métodos de classe

--- oo-sabor-express/model/restaurante.py
+++ oo-sabor-express/model/restaurante.py
@@ -1,20 +1,30 @@
 class Restaurante:
     restaurantes = []
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f"{self.nome} | {self.categoria}"
+        return f"{self._nome} | {self._categoria}"
 
-    def listar_restaurante():
-        for restaurante in restaurantes:
-            print(f'Restaurante {restaurante.nome} | Categoria: {restaurante.categoria} | Ativo: {restaurante.ativo}')
+    @classmethod
+    def listar_restaurante(cls):
+        print(f"{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | Status")
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
 
-restaurante_praca = Restaurante("Praça", "Gourmet")
-restaurante_pizza = Restaurante("Pizza Express", "Italiano")
+    @property
+    def ativo(self):
+        return 'Ativo' if self._ativo else 'Desativado'
+
+    def alternar_estado(self):
+        self._ativo = not self._ativo
+
+restaurante_praca = Restaurante("praça", "gourmet")
+restaurante_praca.alternar_estado()
+restaurante_pizza = Restaurante("pizza express", "italiano")
 
 
 restaurantes =[restaurante_praca, restaurante_pizza]
